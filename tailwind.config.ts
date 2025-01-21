8cb90bbff6f062cdd7082f1f2e0bb8b1b60beb2c
chiisaww <chikasariw@gmail.com>
2025-01-21 09:13:04 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary100: "#C0ED54",
+        primary200: "#DDF813",
+        primary300: "#0B4221",
+        primary400: "#033225",
+        gray:"#F3F3FF",
+        gray100: "#F1F2F1",
+        gray200: "#E6E7E7",
+        gray300: "#BABDBC",
+        gray400: "#8E9492",
+        gray500: "#676E6C",
+        gray600: "#464848",
+        dark: "#141615",
+      },
+
+      // Font custom
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
       },
     },
   },
