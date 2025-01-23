dba12115dc0e1547f2e821b7a71d4409a80cf65f
chikasariw <chikasariw@gmail.com>
2025-01-23 20:12:25 +0700

    hapus sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,5 @@
 import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
-import { AppSidebar } from "@/components/ui/app-sidebar"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
 import "./globals.css";
-import { ModeToggle } from "@/components/ui/mode-toggle";
-import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,29 +26,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${plusJakartaSans.variable} antialiased`}
       >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-              <div className="flex items-center gap-2 px-4">
-                <ModeToggle/>
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                
-              </div>
-            </header>
           <main>
             {children}
           </main>
-          </SidebarInset>
-        </SidebarProvider>
-        </ThemeProvider>
       </body>
     </html>
   );
