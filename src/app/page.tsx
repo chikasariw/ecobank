a98024770570e2b9e485a0211ecc529105c1fa3a
Graschindya <alincindy1@gmail.com>
2025-01-24 21:18:37 +0700

    edit text di landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,9 @@ export default function Home() {
       {/* HERO */}
       <div className="flex flex-col lg:flex-row max-w-screen-xl items-center bg-eb-primary-green-800 justify-center mx-auto p-10 my-4 lg:my-24">
         <div>
-          <h1 className="italic eb-primary-green">Halo, Cindyyyy!</h1> 
+          <h1 className="italic text-eb-primary-ygreen-400">Halo, Cindyyyy!</h1> 
         </div>
       </div>
     </main>
-   
   );
 }
