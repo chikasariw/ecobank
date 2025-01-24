1a8d0c5f458ca7eb067d362778fe3762dd60a96b
ramabintang24 <ramaudaa24@gmail.com>
2025-01-24 21:11:42 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       {/* HERO */}
       <div className="flex flex-col lg:flex-row max-w-screen-xl items-center bg-eb-primary-green-800 justify-center mx-auto p-10 my-4 lg:my-24">
         <div>
-          <h1 className="italic eb-primary-green">Halo, Cindyyyy!</h1> 
+          <h1 className="italic eb-primary-green">Halo, botol yakult!</h1> 
         </div>
       </div>
     </main>
