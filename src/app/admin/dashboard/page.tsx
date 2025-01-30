95ba9b79f7d634bb8e442e6ece7a365b8ebb7e15
chikasariw <chikasariw@gmail.com>
2025-01-30 12:23:48 +0700

    layout dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+import * as React from "react";
 
-export default function Dashboard() {
+import { WelcomeSection } from "./welcome-card"
+import { BalanceCard } from "./balance-card"
+import { TransactionHistory } from "./transaction-history"
+import { StatsSection } from "./chart-card"
+
+export default function DashboardPage() {
   return (
     <div>
-      
+      <div className="lg:flex gap-3">
+        <WelcomeSection />
+        <BalanceCard />
+      </div>
+      <div className="lg:flex gap-3">
+        <TransactionHistory />
+        <StatsSection />
+      </div>
     </div>
   )
 }
---
+++ src/app/admin/dashboard/welcome-card.tsx
@@ -0,0 +1,13 @@
+export function WelcomeSection() {
+    return (
+      <div className="w-full lg:w-2/3 relative overflow-hidden rounded-3xl bg-eb-primary-green-100 p-6 mb-3">
+        <div className="relative z-10">
+          <h1 className="text-2xl font-bold text-eb-primary-ygreen-900">Hai Park Jisungg!</h1>
+          <p className="mt-2 text-sm text-eb-primary-gray-600">
+            Kelola transaksi dan pantau penjualan sampah anorganik secara efisien di platform ini
+          </p>
+        </div>
+      </div>
+    )
+  }
+  
