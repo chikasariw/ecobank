b62e3a563a681b99fc47dc021059fbc8e14d85fa
Graschindya <alincindy1@gmail.com>
2025-01-30 15:00:57 +0700

    fix responsive section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <Header />
 
       {/* HERO */}
-      <div className="flex flex-col lg:flex-row max-w-screen-xl items-center justify-center mx-auto p-10 my-4 lg:my-12">
-        <div className="flex flex-1 flex-col justify-start">
+      <div className="flex flex-col lg:flex-row max-w-screen-xl items-center justify-center mx-auto p-10 my-4 lg:my-6">
+        <div className="flex-1 w-full items-start justify-start">
           <h4 className="text-eb-primary-gray-600 text-sm font-medium uppercase tracking-wide">
             Langkah kecil manfaat besar
           </h4>
@@ -30,10 +30,10 @@ export default function Home() {
             >
               Anorganik?
             </div>
-            Cofika Solusinya!
+            EcoBank Solusinya!
           </h1>
           <h5 className="text-eb-primary-gray-600 text-base font-medium max-w-md mt-12">
-            Tukar, kelola, dan manfaatkan sampahmu dengan mudah melalui Cofika.
+            Tukar, kelola, dan manfaatkan sampahmu dengan mudah melalui EcoBank.
             Yuk, mulai aksi nyatamu hari ini!
           </h5>
           <Button
@@ -49,10 +49,11 @@ export default function Home() {
             </Link>
           </Button>
         </div>
-        <div className="flex flex-1 relative justify-end">
+        
+        <div className="flex flex-1 w-full relative justify-end pt-10 md:pt-10 lg:pt-0">
           <Image
             src="/content/hero-image.png"
-            className=""
+            className="flex justify-end"
             alt="hero image"
             width={450}
             height={450}
@@ -60,7 +61,7 @@ export default function Home() {
           <div className="absolute bottom-3 text-xs text-eb-primary-gray-700 w-40">
             <p>Jangan biarkan sampahmu terbuang percuma.</p>
           </div>
-          <div className="absolute top-12 left-20 w-60 bg-white/60 px-6 py-4 rounded-3xl backdrop-blur-md">
+          <div className="absolute w-60 bg-white/60 px-6 py-4 rounded-3xl backdrop-blur-md top-20 md:top-20 lg:top-12 -left-3 md:left-40 lg:left-20">
             <h6 className="font-bold text-base text-eb-primary-gray-800 ">
               Solusi Anorganik
             </h6>
@@ -79,8 +80,8 @@ export default function Home() {
       </div>
 
       {/* ABOUT */}
-      <div className="flex flex-col lg:flex-row max-w-screen-xl items-center justify-center mx-auto p-10 my-4 lg:my-24">
-        <div className="flex flex-1 relative items-center ">
+      <div className="flex flex-col-reverse lg:flex-row max-w-screen-xl items-center justify-center mx-auto p-10 my-4 lg:my-24">
+        <div className="flex flex-1 w-full relative items-center md:items-end md:justify-end lg:justify-start pt-10 md:pt-10 lg:pt-0">
           <Image
             src="/content/about-image.png"
             className=""
@@ -88,19 +89,19 @@ export default function Home() {
             width={500}
             height={500}
           ></Image>
-          <div className="absolute bottom-3 text-xs text-eb-primary-gray-700 w-40">
+          <div className="absolute bottom-3 lg:left-0 md:left-48 text-xs text-eb-primary-gray-700 w-40">
             <p>Jangan biarkan sampahmu terbuang sia-sia!</p>
           </div>
         </div>
-        <div className="flex flex-1 flex-col justify-start">
+        <div className="flex flex-1 w-full flex-col justify-start">
           <h4 className="text-eb-primary-gray-600 text-sm font-medium uppercase tracking-wide">
             Sampahmu, untukmu!
           </h4>
-          <h1 className="text-eb-primary-gray-800 font-bold text-4xl mt-2">
-            Sampahmu Berharga, Yuk Tukarkan di Cofika!
+          <h1 className="text-eb-primary-gray-800 font-bold text-4xl max-w-md mt-2">
+            Sampahmu Berharga, Yuk Tukarkan di EcoBank!
           </h1>
           <h5 className="text-eb-primary-gray-600 text-base font-medium max-w-md mt-4">
-            Dengan inovasi digital, Cofika bikin urusan sampah jadi lebih mudah!
+            Dengan inovasi digital, EcoBank bikin urusan sampah jadi lebih mudah!
             Tukarkan sampah anorganikmu jadi uang dan bersama-sama kita wujudkan
             lingkungan yang lebih bersih serta bermanfaat untuk semua.
           </h5>
@@ -131,12 +132,12 @@ export default function Home() {
             </h1>
             <h5 className="text-eb-primary-gray-600 text-base font-medium text-center mt-4">
               Jual sampah anorganikmu dengan mudah lewat fitur-fitur digital
-              Cofika.
+              EcoBank.
             </h5>
           </div>
         </div>
         <div className="flex flex-1 relative items-center w-full mt-12">
-          <div className="grid grid-cols-5 gap-4 w-full">
+          <div className="grid lg:grid-cols-5 md:grid-cols-1 gap-4 w-full">
             <div className="col-span-3 bg-eb-primary-green-900 px-6 py-4 rounded-3xl backdrop-blur-md py-6 px-6">
               <h4 className="font-bold text-xl text-eb-primary-gray-800 ">
                 Data Katalog Produk
@@ -196,7 +197,7 @@ export default function Home() {
               Ada pertanyaan? Kami Punya Jawabannya!
             </h1>
             <h5 className="text-eb-primary-gray-600 text-base font-medium text-center mt-4">
-              Temukan jawaban cepat untuk pertanyaan umum tentang Cofika kami.
+              Temukan jawaban cepat untuk pertanyaan umum tentang EcoBank kami.
             </h5>
           </div>
         </div>
@@ -204,7 +205,7 @@ export default function Home() {
           <div className="grid grid-cols-2 gap-4 w-full">
             <div className="flex flex-row bg-eb-primary-gray-100 border-2 border-eb-primary-gray-300 rounded-3xl px-6 py-4 items-center justify-between">
               <h5 className="font-semibold text-eb-primary-green-700">
-                Bagaimana cara mulai jual sampah di Cofika?
+                Bagaimana cara mulai jual sampah di EcoBank?
               </h5>
               <div className="flex h-8 w-8 items-center justify-center rounded-3xl bg-eb-primary-gray-200">
                 <ChevronDown className="h-4" />
@@ -212,7 +213,7 @@ export default function Home() {
             </div>
             <div className="flex flex-row bg-eb-primary-gray-100 border-2 border-eb-primary-gray-300 rounded-3xl px-6 py-4 items-center justify-between">
               <h5 className="font-semibold text-eb-primary-green-700">
-                Apakah ada jenis sampah tertentu yang tidak diterima di Cofika?
+                Apakah ada jenis sampah tertentu yang tidak diterima di EcoBank?
               </h5>
               <div className="flex h-8 w-8 items-center justify-center rounded-3xl bg-eb-primary-gray-200">
                 <ChevronDown className="h-4" />
@@ -236,7 +237,7 @@ export default function Home() {
             </div>
             <div className="flex flex-row bg-eb-primary-gray-100 border-2 border-eb-primary-gray-300 rounded-3xl px-6 py-4 items-center justify-between">
               <h5 className="font-semibold text-eb-primary-green-700">
-                Apakah saya perlu memilah sampah sebelum dijual di Cofika?
+                Apakah saya perlu memilah sampah sebelum dijual di EcoBank?
               </h5>
               <div className="flex h-8 w-8 items-center justify-center rounded-3xl bg-eb-primary-gray-200">
                 <ChevronDown className="h-4" />
@@ -265,10 +266,10 @@ export default function Home() {
         <div className="flex items-center justify-center bg-eb-primary-green-800 rounded-3xl w-full py-20 relative overflow-hidden">
           <div className="flex flex-col justify-center items-center">
             <h1 className="text-eb-primary-gray-100 font-bold text-3xl mt-2 text-center">
-              Cari Tahu Sampah yang Dapat Ditukar di Cofika
+              Cari Tahu Sampah yang Dapat Ditukar di EcoBank
             </h1>
             <p className="max-w-xl text-eb-primary-gray-100 text-base font-regular text-center mt-4">
-              Pelajari sampah yang dapat ditukar di Cofika dan cari tahu
+              Pelajari sampah yang dapat ditukar di EcoBank dan cari tahu
               keuntungan yang bisa kamu peroleh!
             </p>
             <Button
