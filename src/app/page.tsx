08d5eb95651b842b4e42d69d05756e62fb8b7691
Graschindya <alincindy1@gmail.com>
2025-02-17 09:41:50 +0700

    fix section fitur in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,22 +9,68 @@ import Header from "@/components/ui/header";
 import InfiniteScroll from "@/components/ui/infinite-scroll";
 import Footer from "@/components/ui/footer";
 import FAQ from "./faq";
+import Fitur from "./fitur";
+import { Menu, X } from "lucide-react";
+import { useState, useEffect } from "react";
+
+
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+const [visibleSection, setVisibleSection] = useState("");
+const [seenSections, setSeenSections] = useState(new Set());
+
+useEffect(() => {
+  const sections = document.querySelectorAll("section");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !seenSections.has(entry.target.id)) {
+          setVisibleSection(entry.target.id);
+          setSeenSections((prev) => new Set(prev).add(entry.target.id)); // Tandai sudah terlihat
+        }
+      });
+    },
+    { threshold: 0.3 } // Animasi muncul saat 30% dari section terlihat
+  );
+
+  sections.forEach((section) => observer.observe(section));
+
+  return () => {
+    sections.forEach((section) => observer.unobserve(section));
+  };
+}, [seenSections]);
+
+const toggleMenu = () => {
+  setIsOpen(!isOpen);
+};
+
+
   return (
     <main className="bg-white">
       <Header />
 
       {/* HERO */}
-      <div
+      <section
         className="flex flex-col lg:flex-row max-w-screen-xl items-center justify-center scroll-mt-[100px] mx-auto px-10 pt-24 pb-10 my-4 lg:my-10 "
         id="beranda"
       >
         <div className="flex-1 w-full items-start justify-start" id="beranda">
-          <h4 className="text-eb-primary-gray-600 text-sm font-medium uppercase tracking-wide">
+          <h4 className={`text-eb-primary-gray-600 text-sm font-medium uppercase tracking-wide transition-all duration-700 ease-out ${
+              visibleSection === "beranda"
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 -translate-y-5"
+            }`}>
             Langkah kecil manfaat besar
           </h4>
-          <h1 className="text-eb-primary-gray-800 font-bold text-4xl md:text-6xl mt-2 md:mt-4">
+          <h1
+            className={`text-eb-primary-gray-800 font-bold text-4xl md:text-6xl mt-2 md:mt-4 transition-all duration-700 ease-out ${
+              visibleSection === "beranda"
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 -translate-y-5"
+            }`}
+          >
             Sampah
             <div
               className="text-4xl md:text-6xl font-bold bg-gradient-to-r from-eb-primary-green-600 to-eb-primary-green-400 bg-clip-text text-transparent py-1 md:py-2"
@@ -35,12 +83,20 @@ export default function Home() {
             </div>
             EcoBank Solusinya!
           </h1>
-          <h5 className="text-eb-primary-gray-600 text-base font-medium max-w-md mt-6 md:mt-12">
+          <h5 className={`text-eb-primary-gray-600 text-base font-medium max-w-md mt-6 md:mt-12 transition-all duration-700 delay-200 ease-out ${
+              visibleSection === "beranda"
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 -translate-y-5"
+            }`}>
             Tukar, kelola, dan manfaatkan sampahmu dengan mudah melalui EcoBank.
             Yuk, mulai aksi nyatamu hari ini!
           </h5>
           <Button
-            className="w-44 mt-6 md:mt-12 ps-4 py-4"
+            className={`w-44 mt-6 md:mt-12 ps-4 py-4 transition-all duration-700 delay-200 ease-out ${
+              visibleSection === "beranda"
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 -translate-y-5"
+            }`}
             variant="primarycustom"
             asChild
           >
@@ -56,16 +112,22 @@ export default function Home() {
         <div className="flex flex-1 w-full relative justify-end pt-10 md:pt-10 lg:pt-0">
           <Image
             src="/content/hero-image.png"
-            className="w-full max-w-xs md:max-w-md lg:max-w-md h-auto flex justify-end"
+            className={`w-full max-w-xs md:max-w-md lg:max-w-md h-auto flex justify-end transition-transform duration-1000 delay-200 ease-out ${visibleSection === "beranda" ? "opacity-100 scale-100" : "opacity-0 scale-90"}`}
             alt="hero image"
             width={450}
             height={450}
           />
 
-          <div className="absolute -right-4 md:-right-0  bottom-1 md:bottom-3 text-[10px] md:text-xs text-eb-primary-gray-700 w-32 md:w-40">
+          <div className={`absolute -right-4 md:-right-0  bottom-1 md:bottom-3 text-[10px] md:text-xs text-eb-primary-gray-700 w-32 md:w-40 transition-all duration-700 delay-300 ${
+            visibleSection === "beranda" ? "opacity-100 scale-100" : "opacity-0 scale-90"
+          }`}>
             <p>Jangan biarkan sampahmu terbuang percuma.</p>
           </div>
-          <div className="absolute w-52 md:w-60 bg-white/60 px-6 py-4 rounded-3xl backdrop-blur-md top-20 md:top-20 lg:top-12 -left-3 md:left-40 lg:left-20">
+          <div className={`absolute w-52 md:w-60 bg-white/60 px-6 py-4 rounded-3xl backdrop-blur-md top-20 md:top-20 lg:top-12 -left-3 md:left-40 lg:left-20 transition-all duration-700 ease-out ${
+              visibleSection === "beranda"
+                ? "opacity-100 translate-x-0"
+                : "opacity-0 -translate-x-5"
+            }`}>
             <h6 className="font-bold text-md md:text-base text-eb-primary-gray-800 ">
               Solusi Anorganik
             </h6>
@@ -74,7 +136,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-      </div>
+      </section>
 
       {/* BRAND */}
       <div className="flex flex-row max-w-screen-auto items-center justify-center mx-auto my-4 ">
@@ -84,19 +146,21 @@ export default function Home() {
       </div>
 
       {/* ABOUT */}
-      <div
+      <section
         className="flex flex-col-reverse lg:flex-row max-w-screen-xl items-center justify-center scroll-mt-[120px] mx-auto p-10 my-4 lg:my-12"
         id="tentang-kami"
       >
         <div className="flex flex-1 w-full relative items-center md:items-end md:justify-end lg:justify-start pt-10 md:pt-10 lg:pt-0">
           <Image
             src="/content/about-image.png"
-            className=""
+            className={`transition-transform duration-1000 delay-200 ease-out ${ visibleSection === "tentang-kami" ? "opacity-100 scale-100" : "opacity-0 scale-90"}`}
             alt="hero image"
             width={500}
             height={500}
           ></Image>
-          <div className="absolute bottom-0 md:bottom-3 lg:left-0 md:left-48 text-[10px] md:text-xs text-eb-primary-gray-700 w-28 md:w-40">
+          <div className={`absolute bottom-0 md:bottom-3 lg:left-0 md:left-48 text-[10px] md:text-xs text-eb-primary-gray-700 w-28 md:w-40  transition-all duration-700 delay-300 ${
+            visibleSection === "tentang-kami" ? "opacity-100 scale-100" : "opacity-0 scale-90"
+          }`}>
             <p>Jangan biarkan sampahmu terbuang sia-sia!</p>
           </div>
         </div>
@@ -125,107 +189,10 @@ export default function Home() {
             </Link>
           </Button>
         </div>
-      </div>
+      </section>
 
-      {/* FITUR */}
-      <div
-        className="flex flex-col max-w-screen-xl items-center justify-center scroll-mt-[110px] mx-auto p-10 my-4 lg:my-12"
-        id="fitur"
-      >
-        <div className="flex flex-1 relative items-center ">
-          <div className="flex flex-1 flex-col justify-center">
-            <h4 className="text-eb-primary-gray-600 text-sm font-medium uppercase tracking-wide text-center">
-              Fitur ecobank
-            </h4>
-            <h1 className="text-eb-primary-gray-800 font-bold text-3xl md:text-4xl text-center mt-2 ">
-              Jual Sampah Jadi Lebih Praktis!
-            </h1>
-            <h5 className="text-eb-primary-gray-600 text-base font-medium text-center mt-4">
-              Jual sampah anorganikmu dengan mudah lewat fitur-fitur digital
-              EcoBank.
-            </h5>
-          </div>
-        </div>
-        <div className="flex flex-1 relative items-center w-full mt-12">
-          <div className="grid lg:grid-cols-7 md:grid-cols-1 gap-4 w-full">
-            <div className="col-span-4 relative bg-gradient-to-r from-eb-primary-gray-200 via-eb-primary-green-100 to-eb-primary-gray-200 rounded-3xl backdrop-blur-lg overflow-hidden py-6 px-6 ">
-              {/* Konten */}
-              <div className="relative z-10">
-                <h4 className="font-bold text-xl text-eb-primary-gray-800 ">
-                  Data Katalog Produk
-                </h4>
-                <p className="text-base text-eb-primary-gray-600 mt-2">
-                  Upgrade to a modern cashier system that helps you streamline
-                  operations, enhance customer experience, and boost revenue
-                  with ease. boost revenue with ease.
-                </p>
-                <div className="bg-white/20 px-6 py-4 rounded-3xl backdrop-blur-md h-40 mt-4"></div>
-              </div>
-            </div>
-            <div className="lg:col-span-3 col-span-4 relative bg-gradient-to-r from-eb-primary-green-700 via-eb-primary-green-600 to-eb-primary-green-500 rounded-3xl backdrop-blur-md overflow-hidden py-6 px-6">
-              {/* Radial Background */}
-              <div className="absolute inset-0 w-full h-full z-0">
-                <div className="radial-yellow-card-right w-full h-full"></div>
-              </div>
-
-              {/* Konten */}
-              <div className="relative z-10">
-                <div className="bg-white/20 px-6 py-4 rounded-2xl backdrop-blur-md h-36"></div>
-                <h4 className="font-bold text-xl text-eb-primary-gray-200 mt-4">
-                  Data Katalog Produk
-                </h4>
-                <p className="text-base text-eb-primary-gray-200 mt-2">
-                  Upgrade to a modern cashier system that helps you streamline
-                  operations, enhance customer experience, and boost revenue
-                  with ease.
-                </p>
-              </div>
-            </div>
-
-            <div className="lg:col-span-3 col-span-4 relative bg-gradient-to-r from-eb-primary-green-700 via-eb-primary-green-600 to-eb-primary-green-500 rounded-3xl backdrop-blur-md py-6 px-6 overflow-hidden order-last lg:order-none">
-              {/* Radial Background */}
-              <div className="absolute inset-0 w-full h-full z-0">
-                <div className="radial-yellow-card-right w-full h-full"></div>
-                <div className="radial-yellow-card w-full h-full"></div>
-              </div>
-
-              {/* Konten */}
-              <div className="relative z-10">
-                <div className="bg-white/20 px-6 py-4 rounded-2xl backdrop-blur-md h-36"></div>
-                <h4 className="font-bold text-xl text-eb-primary-gray-200 mt-4">
-                  Data Katalog Produk
-                </h4>
-                <p className="text-base text-eb-primary-gray-200 mt-2">
-                  Upgrade to a modern cashier system that helps you streamline
-                  operations, enhance customer experience, and boost revenue
-                  with ease.
-                </p>
-              </div>
-            </div>
-
-            <div className="col-span-4 relative bg-gradient-to-r from-eb-primary-green-100 via-eb-primary-gray-100 to-eb-primary-green-100 rounded-3xl backdrop-blur-md overflow-hidden py-6 px-6">
-              {/* Radial Background */}
-              {/* <div className="absolute inset-0 w-full h-full z-0">
-                <div className="radial-green-card w-full h-full"></div>
-                <div className="radial-yellow-card w-full h-full"></div>
-              </div> */}
-
-              {/* Konten */}
-              <div className="relative z-10">
-                <h4 className="font-bold text-xl text-eb-primary-gray-800 ">
-                  Data Katalog Produk
-                </h4>
-                <p className="text-base text-eb-primary-gray-600 mt-2">
-                  Upgrade to a modern cashier system that helps you streamline
-                  operations, enhance customer experience, and boost revenue
-                  with ease. boost revenue with ease.
-                </p>
-                <div className="bg-white/20 px-6 py-4 rounded-3xl backdrop-blur-md h-40 mt-4"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      { /* Fitur */}
+      <Fitur />
 
       {/* FAQ */}
       <FAQ />
