40944f32d15dd31324a9a8baa6c25d94adea1343
ramabintang24 <ramaudaa24@gmail.com>
2025-04-09 10:56:39 +0700

    fix login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,14 @@
-import Login from "./client"; 
+import Login from "./client";
+
+export const dynamic = "force-dynamic";
 
 export async function generateMetadata() {
   return {
     title: "Login - EcoBank.",
     description: "Halaman Login EcoBank.",
-  }
+  };
 }
 
 export default function LoginPage() {
-  return <Login/>;
+  return <Login />;
 }
